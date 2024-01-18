99570fb9edebb75256148e9d0e8d6678cbaacb90
setiana <tesmojones@gmail.com>
2024-01-18 13:59:23 +0700

    add crud user

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html class="h-100" lang="en">
 
@@ -9,13 +8,14 @@
     <title>Quixlab - Bootstrap Admin Dashboard Template by Themefisher.com</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon.png">
+    <link href="/asset/plugins/sweetalert/css/sweetalert.css" rel="stylesheet">
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
     <link href="/asset/css/style.css" rel="stylesheet">
-    
+
 </head>
 
 <body class="h-100">
-    
+
     <!--*******************
         Preloader start
     ********************-->
@@ -37,19 +37,16 @@
                     <div class="form-input-content">
                         <div class="card login-form mb-0">
                             <div class="card-body pt-5">
-                                <a class="text-center" href=""> <h4>Silahkan Login</h4></a>
-        
+                                <a class="text-center" href="">
+                                    <h4>Silahkan Login</h4>
+                                </a>
+
                                 <form class="mt-5 mb-5 login-input" method="POST" action="{{ route('authenticate') }}">
                                     @csrf
-                                    @error('email')
-                                    <div class="form-group">
-                                        <strong style="color:red">{{ $message }}</strong>
-                                    </div>
-                                    @enderror
                                     <div class="form-group">
                                         <input type="email" class="form-control" placeholder="Email" name="email" required>
                                     </div>
-                                
+
                                     <div class="form-group">
                                         <input type="password" class="form-control" placeholder="Password" name="password" required>
                                     </div>
@@ -62,9 +59,7 @@
             </div>
         </div>
     </div>
-    
 
-    
 
     <!--**********************************
         Scripts
@@ -74,10 +69,15 @@
     <script src="/asset/js/settings.js"></script>
     <script src="/asset/js/gleek.js"></script>
     <script src="/asset/js/styleSwitcher.js"></script>
+
+    <script src="/asset/plugins/sweetalert/js/sweetalert.min.js"></script>
+    <script src="/asset/plugins/sweetalert/js/sweetalert.init.js"></script>
+
+    @error('email')
+    <script>
+        sweetAlert("{{ $message }}", "{{ $message }}", "error")
+
+    </script>
+    @enderror
 </body>
 </html>
-
-
-
-
-
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\UserController;
  
 
 Route::controller(AuthController::class)->group(function () {
@@ -13,6 +13,12 @@ Route::controller(AuthController::class)->group(function () {
 });
 
 Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/user', [UserController::class, 'index']);
+    Route::post('/user/store', [UserController::class, 'store']);
+    Route::post('/user/update/{id}', [UserController::class, 'update']);
+    Route::get('/user/destroy/{id}', [UserController::class, 'destroy']);
+    
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 });
